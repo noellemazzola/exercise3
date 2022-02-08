8d04fc61174fe82a7b6c7f267684660d25cae623
noellemazzola <97685560+noellemazzola@users.noreply.github.com>
2022-02-08 16:11:37 -0500

    February 8, 2022 4:11 PM

--- exercises.js
+++ exercises.js
@@ -1,6 +1,11 @@
 //3.1 flattening(list)
+ function repeat(n, action) {
+     for(let i = 0; i < n; i++) {
+         action(i);
+     }
+ }
 
-
+ repeat(3, console.log);
 //3.2 loop(value, test, update, body)
 
 
