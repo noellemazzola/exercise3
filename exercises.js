26e043a86b8c87d9be877a59f8fb1ee7aa5b80ef
noellemazzola <97685560+noellemazzola@users.noreply.github.com>
2022-02-08 16:34:45 -0500

    February 8, 2022 4:34 PM

--- exercises.js
+++ exercises.js
@@ -1,5 +1,9 @@
 //3.1 flattening(list)
-let arrays = [[1, 2, 3,], [4, 5], [6] ];
+let array1 = [1, 2, 3];
+let array2 = [4, 5];
+let array3 = [6];
+let array4 = array1.concat(array2, array3);
+console.log(array4);
 
 //3.2 loop(value, test, update, body)
 
