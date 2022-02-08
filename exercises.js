61298e7d907ad3d74fc8b86155565eb57054c114
noellemazzola <97685560+noellemazzola@users.noreply.github.com>
2022-02-08 16:58:57 -0500

    February 8, 2022 4:58 PM

--- exercises.js
+++ exercises.js
@@ -1,10 +1,11 @@
 //3.1 flattening(list)
-let array1 = [1, 2, 3];
-let array2 = [4, 5];
-let array3 = [6];
-let array4 = array1.concat(array2)
-console.log(array4);
+let arrays = [[1, 2, 3], [4, 5], [6]]
 
+export function flattening(inputList) {
+     return inputList.reduce((array1, array2) => array1.concat(array2))
+}
+
+console.log(flattening(arrays))
 //3.2 loop(value, test, update, body)
 
 
