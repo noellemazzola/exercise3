9c25fcab6f352aae0f78349cca4b9f8a43ea363e
noellemazzola <97685560+noellemazzola@users.noreply.github.com>
2022-02-08 16:35:19 -0500

    February 8, 2022 4:35 PM

--- exercises.js
+++ exercises.js
@@ -2,7 +2,7 @@
 let array1 = [1, 2, 3];
 let array2 = [4, 5];
 let array3 = [6];
-let array4 = array1.concat(array2, array3);
+let array4 = array1.concat(array2);
 console.log(array4);
 
 //3.2 loop(value, test, update, body)
