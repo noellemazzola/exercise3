dbcd3950120a1ffe84a1fb8250809fbd6e203c45
noellemazzola <97685560+noellemazzola@users.noreply.github.com>
2022-02-08 14:04:30 -0500

    February 8, 2022 2:04 PM

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # exercise3
+
+## favorite quote
+
+"No one can do everything, but everyone can do something" - Max Lucado
---
+++ index.js
@@ -0,0 +1,8 @@
+let quotes = [`"No one can do  everything, but everyone can do something" -Max Lucado`, 
+`"Believe in yourselves. Dream. Try. Do Good." -George Feeny`, 
+`"The future belongs to those who believe in the beauty of their dreams." - Eleanor Roosevelt`]
+//console.log(Math.random())
+//console.log(3 * Math.random())
+//console.log (Math.floor((3 * Math.random())))
+let index = Math.floor((quotes.length * Math.random()))
+console.log(quotes[index])
