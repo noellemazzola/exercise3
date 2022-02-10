4c3dda0303e7c8b8e77edc3e920e6c49f8fcdb65
noellemazzola <97685560+noellemazzola@users.noreply.github.com>
2022-02-09 22:30:12 -0500

    February 9, 2022 10:30 PM

--- exercises.js
+++ exercises.js
@@ -1,11 +1,14 @@
 //3.1 flattening(list)
 // look at about 1 hour and 35 minutes for help
+
 let arrays = [[1, 2, 3], [4, 5], [6]]
 export function flattening(list) {
     return list.reduce((previousValue, currentValue) => previousValue.concat(currentValue) )
 }
 console.log(flattening(arrays))
+
 //3.2 loop(value, test, update, body)
+
 
 //3.3 everyLoop(array, test)
 
